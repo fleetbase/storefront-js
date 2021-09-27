833f30fd7493b97104a5d272221e7d629e7144a0
Ronald A. Richardson <me@ron.dev>
2021-09-27 17:44:43 +0800

    updated rollup config and push dist bundles

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,68 @@
 // rollup.config.js
 import { terser } from 'rollup-plugin-terser';
-import { eslint } from 'rollup-plugin-eslint';
-import resolve from 'rollup-plugin-node-resolve';
+// import { eslint } from 'rollup-plugin-eslint';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import babel from '@rollup/plugin-babel';
+import pkg from './package.json';
 
-export default {
-	input: 'src/storefront.js',
-	output: {
-		format: 'umd',
-		file: 'dist/bundle.js',
-		name: '@fleetbase/storefront'
-	},
-	watch: {
-		exclude: ['node_modules/**'],
-    	include: ['lib/**']
-	},
-	plugins: [
-	    resolve({
-	      browser: true,
-	      modulesOnly: true
-	    }),
-		terser(),
-		// eslint()
-	]
-};
+const input = ['src/storefront.js'];
+
+export default [
+    {
+        // umd
+        input,
+        plugins: [
+            nodeResolve({
+                browser: true,
+                modulesOnly: true,
+            }),
+            babel({
+				babelHelpers: 'bundled',
+			}),
+            terser(),
+        ],
+        output: [
+            {
+                file: `dist/${pkg.name}.min.js`,
+                format: 'umd',
+                name: '@fleetbase/storefront',
+                esModule: false,
+                exports: 'named',
+                sourcemap: true,
+            },
+        ],
+        watch: {
+            exclude: ['node_modules/**'],
+            include: ['lib/**'],
+        },
+    },
+    {
+        // esm and cjs
+        input,
+        plugins: [
+            nodeResolve({
+                browser: true,
+                modulesOnly: true,
+            }),
+			babel({
+				babelHelpers: 'bundled',
+			}),
+			terser()
+        ],
+        output: [
+            {
+                dir: 'dist/esm',
+                format: 'esm',
+                exports: 'named',
+                sourcemap: true,
+            },
+            {
+                dir: 'dist/cjs',
+                format: 'cjs',
+                exports: 'named',
+                sourcemap: true,
+            },
+        ],
+		external: ['axios']
+    },
+];
